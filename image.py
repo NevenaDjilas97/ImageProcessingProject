aba01a035588e60e78aa4d79449101001312ab6b
Aleksandar <aleksandar.jankovic456@gmail.com>
2021-03-24 23:35:49 +0100

    first_version_of_image_class

---
+++ image.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 20:25:12 2021
+
+@author: aleksandar
+"""
+import numpy as np
+from  PIL import Image
+
+class MyImage:
+    
+    
+    def __init__(self, image_path):
+        image=Image.open(image_path)
+        self.image_path=image_path
+        self.arr=np.array(image)
+        self.height=self.arr[0].size//3
+        self.width=self.arr.size//3//self.height
+      
+      
+    
+    def getSize(self):
+        return (self.width,self.height)
+    
+    def open(image_path):
+        return  MyImage(image_path)
+        
+    def save(self,save_image_path):
+       image=Image.fromarray(self.arr)
+       image.save(save_image_path,quality=80,optimize=True, progressive=False)
+    
+    def getpixel(self,shape):
+        w=shape[0]
+        h=shape[1]
+        r,g,b=self.arr[w][h]
+        return (r,g,b)
+        
+    def putpixel(self,shape, rgb):
+        w,h=shape[0],shape[1]
+        r,g,b=rgb[0],rgb[1],rgb[2]
+        self.arr[w][h][0]=r
+        self.arr[w][h][1]=g
+        self.arr[w][h][2]=b
+    
+
+    def convert(self):
+        print('soon')
+        
+    def histogram(self):
+        print('soon')
+    
+    
+image=MyImage.open('slika.jpeg')
+w,h=image.getSize()
+for i in range(0,h):
+    for j in range (0,w):
+        image.getpixel((j,i))
